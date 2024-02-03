f8d6f710e08834dfe66dbaf66d3297747a5069a3
Biggestmahe <157534365+Biggestmahe@users.noreply.github.com>
2024-02-03 21:34:38 +0000

    Update Task1C.py

--- Task1C.py
+++ Task1C.py
@@ -1,4 +1,9 @@
 from floodsystem.geo import stations_within_radius
 from floodsystem.stationdata import build_station_list
 
-print(stations_within_radius(build_station_list(),(52.2053, 0.1218), 10))
+near_stations=stations_within_radius(build_station_list(),(52.2053, 0.1218), 10)
+output = []
+for i in near_stations:
+    output.append(i.name)
+output.sort()
+print(output)
