0b72419ffce5501bbb3eec020620678914d11a99
Biggestmahe <157534365+Biggestmahe@users.noreply.github.com>
2024-02-03 14:41:45 +0000

    1C

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -9,9 +9,11 @@ geographical data.
 from .utils import sorted_by_key  # noqa
 
 
+#finding which stations are within radius r from a specific centre 
 def stations_within_radius(stations, centre, r):
     within = []
     for station in stations :
-        if ((station.coord[0] - centre[0])**2 + (station.coord[1] - centre[1])**2 ) <= r :
+        #this if statement check the distance
+        if ((station.coord[0] - centre[0])**2 + (station.coord[1] - centre[1])**2 ) <= r**2 :
             within.append(station)
     return within
