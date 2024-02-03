8d8fbcec03dc7532f6c50d22e116bcaca348cf4a
Biggestmahe <157534365+Biggestmahe@users.noreply.github.com>
2024-02-03 21:33:51 +0000

    Update geo.py

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -5,22 +5,40 @@
 geographical data.
 
 """
+from haversine import haversine
 import numpy as np
-#from utils import sorted_by_key  # noqa
+from .utils import sorted_by_key  # noqa
 #task 1B
 def stations_by_distance(stations, p):
     statdist=[]
     for station in stations:
-        distance=2*6370*np.arcsin(np.sqrt(np.sin((station.coordinate[0]-p[0])/2)**2+np.cos(station.coordinate[0])+np.cos(p[0])*np.sin(station.coordinate[1]-p[1])/2))
+        distance= haversine(station.coord, p)
         statdist.append((station,distance))
         statdist=sorted_by_key(statdist,i=1)
-        return statdist
+    return statdist
 
 #finding which stations are within radius r from a specific centre 
 def stations_within_radius(stations, centre, r):
     within = []
     distances= stations_by_distance(stations, centre)
     for i in distances :
-        if i[1] < r :
-            within.append[i[0]]
-    return within.sort()
+        
+        if i[1] <= r :
+            within.append(i[0])
+    return within
+
+def rivers_with_station(stations):
+    rivers = set()
+    for station in stations:
+        if station.measure_id == None :
+            pass
+        else:
+            rivers.add(station.river)
+    return rivers
+
+def stations_by_river(stations):
+    rivers = list(rivers_with_station(stations))
+    station_river = {key: [] for key in rivers}
+    for station in stations:
+        station_river[station.river].append(station.name)
+    return station_river
