3abb8e50ac2bb0e40a07ca5bd34d255b30d17752
Biggestmahe <157534365+Biggestmahe@users.noreply.github.com>
2024-02-03 15:30:51 +0000

    1C

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -18,9 +18,8 @@ def stations_by_distance(stations, p):
 #finding which stations are within radius r from a specific centre 
 def stations_within_radius(stations, centre, r):
     within = []
-    for station in stations :
-        #this if statement check the distance
-        if ((station.coord[0] - centre[0])**2 + (station.coord[1] - centre[1])**2 ) <= r**2 :
-            within.append(station)
-    return within
-#ewfrfsd
+    distances= stations_by_distance(stations, centre)
+    for i in distances :
+        if i[1] < r :
+            within.append[i[0]]
+    return within.sort()
