cd392a7c99ccf8ec34a0d1ddbc5507a9e16ccefe
Biggestmahe <157534365+Biggestmahe@users.noreply.github.com>
2024-02-03 21:34:20 +0000

    Update Task1B.py

--- Task1B.py
+++ Task1B.py
@@ -1,3 +1,8 @@
 import floodsystem.geo as geo
-import Task1A as IA 
-print(geo.stations_by_distance(IA.run(),(52.2053, 0.1218))[:10])
+from floodsystem.stationdata import build_station_list
+
+lis=geo.stations_by_distance(build_station_list(),(52.2053, 0.1218))[:10]
+output = []
+for i in lis:
+    output.append((i[0].name , i[0].town , i[1]))
+print(output)
