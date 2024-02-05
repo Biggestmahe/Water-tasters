ec2214edff4755494288f9ddad6b7db456e825c3
Biggestmahe <157534365+Biggestmahe@users.noreply.github.com>
2024-02-05 21:24:11 +0000

    Test 1C and 1D fixed

--- Task1C.py
+++ Task1C.py
@@ -1,9 +1,23 @@
-from floodsystem.geo import stations_within_radius
-from floodsystem.stationdata import build_station_list
+# Copyright (C) 2018 Garth N. Wells
+#
+# SPDX-License-Identifier: MIT
 
-near_stations=stations_within_radius(build_station_list(),(52.2053, 0.1218), 10)
-output = []
-for i in near_stations:
-    output.append(i.name)
-output.sort()
-print(output)
+from floodsystem.stationdata import build_station_list
+from floodsystem.geo import stations_within_radius
+
+def run():
+    """Requirements for Task 1C"""
+
+    # Build list of stations
+    stations = build_station_list()
+    #using the function
+    near_stations=stations_within_radius(stations,(52.2053, 0.1218), 10)
+    output = []
+    for i in near_stations:
+        output.append(i.name)
+    output.sort()
+    print(output)
+
+if __name__ == "__main__":
+    print("*** Task 1C: CUED Part IA Flood Warning System ***")
+    run()
--- Task1D.py
+++ Task1D.py
@@ -2,14 +2,35 @@ from floodsystem.geo import rivers_with_station
 from floodsystem.stationdata import build_station_list
 from floodsystem.geo import stations_by_river
 
-rivers = list(rivers_with_station(build_station_list()))
-rivers.sort()
-print(str(len(rivers)) + " stations. First 10: "+ str(rivers[:10]))
 
-river2 = stations_by_river(build_station_list())
-River_Aire= river2["River Aire"]
-River_Cam= river2["River Cam"]
-River_Thames= river2["River Thames"]
-print(River_Aire)
-print(River_Cam)
-print(River_Thames)
+# Copyright (C) 2018 Garth N. Wells
+#
+# SPDX-License-Identifier: MIT
+
+from floodsystem.geo import rivers_with_station
+from floodsystem.stationdata import build_station_list
+from floodsystem.geo import stations_by_river
+
+
+def run():
+    """Requirements for Task 1D"""
+
+    # Build list of stations
+    stations = build_station_list()
+
+    rivers = list(rivers_with_station(stations))
+    rivers.sort()
+    print(str(len(rivers)) + " stations. First 10: "+ str(rivers[:10]))
+
+    river2 = stations_by_river(build_station_list())
+    River_Aire= river2["River Aire"]
+    River_Cam= river2["River Cam"]
+    River_Thames= river2["River Thames"]
+    print("River Aire: "+ River_Aire)
+    print("River Cam"+ River_Cam)
+    print("River Thames"+ River_Thames)
+
+
+if __name__ == "__main__":
+    print("*** Task 1D: CUED Part IA Flood Warning System ***")
+    run()
