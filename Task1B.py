79285db413863c18aa0070e2cac787d0bc9761c0
Biggestmahe <157534365+Biggestmahe@users.noreply.github.com>
2024-02-05 21:18:13 +0000

    Task1B check fixed

--- Task1B.py
+++ Task1B.py
@@ -1,8 +1,32 @@
-import floodsystem.geo as geo
-from floodsystem.stationdata import build_station_list
 
-lis=geo.stations_by_distance(build_station_list(),(52.2053, 0.1218))[:10]
-output = []
-for i in lis:
-    output.append((i[0].name , i[0].town , i[1]))
-print(output)
+# Copyright (C) 2018 Garth N. Wells
+#
+# SPDX-License-Identifier: MIT
+
+from floodsystem.stationdata import build_station_list
+import floodsystem.geo as geo
+
+
+
+def run():
+    """Requirements for Task 1B"""
+
+    # Build list of stations
+    stations = build_station_list()
+    closest=geo.stations_by_distance(stations,(52.2053, 0.1218))[:10]
+    furthest= geo.stations_by_distance(stations,(52.2053, 0.1218))[10:]
+    furthest.reverse()
+    output = []
+    for i in closest:
+        output.append((i[0].name , i[0].town , i[1]))
+    print("Closests are: "+ str(output))
+    output = []
+    for i in furthest:
+        output.append((i[0].name , i[0].town , i[1]))
+    print("Furthests are "+ str(output))
+
+
+
+if __name__ == "__main__":
+    print("*** Task 1B: CUED Part IA Flood Warning System ***")
+    run()
