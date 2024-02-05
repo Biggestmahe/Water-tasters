0544271ea001e2f6041ac6d9e7d4e0e0b683dc9c
Biggestmahe <157534365+Biggestmahe@users.noreply.github.com>
2024-02-05 21:07:09 +0000

    comments on 1B to 1D

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -8,7 +8,10 @@ geographical data.
 from haversine import haversine
 import numpy as np
 from .utils import sorted_by_key  # noqa
-#task 1B
+
+#task 1B finding the distances between every station and a coordinate this can be done by using the in built
+#function of haversine. The inputs is the stations(MonitoringStation) and coordinate of p (float, float) and 
+#the output is a list of (MonitoringStation, float)
 def stations_by_distance(stations, p):
     statdist=[]
     for station in stations:
@@ -17,7 +20,9 @@ def stations_by_distance(stations, p):
         statdist=sorted_by_key(statdist,i=1)
     return statdist
 
-#finding which stations are within radius r from a specific centre 
+#finding which stations are within radius r from a specific centre. the stations_by_distance has been used 
+#to find all the distances and then got filter to which is less than input r. inputs are stations(MonitoringStation), 
+# centre(float,float) and r(float) and the output is a list of stations(MonitoringStation).
 def stations_within_radius(stations, centre, r):
     within = []
     distances= stations_by_distance(stations, centre)
@@ -27,6 +32,8 @@ def stations_within_radius(stations, centre, r):
             within.append(i[0])
     return within
 
+# This function represents the rivers which there has been atleast ono station in them, the output is a set which
+#helps to do not recount any river if two or more station pass it
 def rivers_with_station(stations):
     rivers = set()
     for station in stations:
@@ -36,6 +43,7 @@ def rivers_with_station(stations):
             rivers.add(station.river)
     return rivers
 
+#this function uses rivers_with_station and as a dictionary gives us which stations passes through any river(key)
 def stations_by_river(stations):
     rivers = list(rivers_with_station(stations))
     station_river = {key: [] for key in rivers}
