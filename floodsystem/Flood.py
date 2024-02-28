703fa1c8d3669bd67eb52231f202e4d2131d0967
Owen Marlovits <om399@cam.ac.uk>
2024-02-28 16:38:06 +0000

    2b xtra

--- floodsystem/Flood.py
+++ floodsystem/Flood.py
@@ -6,7 +6,7 @@ def stations_level_over_threshold(stations, tol):
     for station in stations:
         if relative_water_level(station)!=None:
             if relative_water_level(station)>=tol:
-                x.append(tuple(station,relative_water_level(station)))
+                x.append((station,relative_water_level(station)))
     x=sorted_by_key(x,1,True)
     return x
 
