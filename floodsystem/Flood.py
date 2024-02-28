bb5a15c6bfa85ac807752fe45f95a531d8715a6b
Biggestmahe <157534365+Biggestmahe@users.noreply.github.com>
2024-02-28 16:13:58 +0000

    2C

--- floodsystem/Flood.py
+++ floodsystem/Flood.py
@@ -1,5 +1,7 @@
 from station import relative_water_level
 from .utils import sorted_by_key
+import statindata
+
 def stations_level_over_threshold(stations, tol):
     x=list()
     for station in stations:
@@ -8,3 +10,12 @@ def stations_level_over_threshold(stations, tol):
                 x.append(tuple(station,relative_water_level(station)))
     sorted_by_key(x,1,True)
     return x
+
+import statindata
+
+def stations_highest_rel_level(stations, N):
+    touples= stations_level_over_threshold(stations, 0)[:N]
+    station_list= []
+    for i in touples:
+        station_list.append(i[0])
+    return station_list
