7071f5e21d6b50cdf845ea1084b053f09086aa94
Owen Marlovits <om399@cam.ac.uk>
2024-02-28 16:12:43 +0000

    2b redo

--- 2B.py
+++ 2B.py
@@ -2,4 +2,9 @@ from floodsystem.Flood import stations_level_over_threshold
 from floodsystem.stationdata import build_station_list
 def run():
     stations = build_station_list()
-    
+    tol=0.8
+    print(stations_level_over_threshold(stations,tol))
+
+if __name__=="main":
+    print("*** Task 1D: CUED Part IA Flood Warning System ***")
+    run()
