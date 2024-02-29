68a4b571af5e49420e6c5ed4e7551a12af5eb161
Owen Marlovits <om399@cam.ac.uk>
2024-02-29 10:40:51 +0000

    2b test

--- test_flood.py
+++ test_flood.py
@@ -5,7 +5,7 @@ def test_statoverthresh():
     station1= MonitoringStation(0,0,"station 1",(0,0),(1.2,2.2),"river 1","town 1")
     station2= MonitoringStation(0,0,"station 2",(10,0),(None,0),"river 1","town 2")
     station1.latest_level=2.2
-    assert stations_level_over_threshold(station1,0.8)==(station1,1)
+    assert stations_level_over_threshold([station1,station2],0.8)==[(station1, 1.0)]
     station1.latest_level=1.2
-    assert stations_level_over_threshold(station1,0.8)==None
-test_statoverthresh
+    assert stations_level_over_threshold([station1,station2],0.8)==[]
+test_statoverthresh()
