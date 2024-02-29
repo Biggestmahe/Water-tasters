5a867f8d472ebd17e8eb7a74bc16a4dfc6a9c334
bigmahe03 <126089416+bigmahe03@users.noreply.github.com>
2024-02-29 15:40:14 +0000

    sda

--- test_flood.py
+++ test_flood.py
@@ -7,8 +7,7 @@ def test_statoverthresh():
     station2= MonitoringStation(0,0,"station 2",(10,0),(None,0),"river 1","town 2")
     station1.latest_level=2.2
     assert stations_level_over_threshold([station1,station2],0.8)==[(station1, 1.0)]
-    station1.latest_level=1.2
-    assert stations_level_over_threshold([station1,station2],0.8)==[]
+
 def test_stations_highest_rel_level():
     station1= MonitoringStation(0,0,"station 1",(0,0),(1.2,2.2),"river 1","town 1")
     station2= MonitoringStation(0,0,"station 2",(10,0),(None,0),"river 1","town 2")
