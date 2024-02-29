472df4d9e9196454cdad9c85ff315b65cb3f2cb7
bigmahe03 <126089416+bigmahe03@users.noreply.github.com>
2024-02-29 15:37:22 +0000

    tests completed

--- test_analysis.py
+++ test_analysis.py
@@ -1,10 +1,12 @@
 from floodsystem.analysis import polyfit 
 import math
 import datetime
-dates = [datetime.datetime(2020, 5, 17), datetime.datetime(2020, 5, 18),datetime.datetime(2020, 5, 19),datetime.datetime(2020, 5, 20) ]
-levels=[0,1,2,3]
-function = polyfit(dates,levels, 1)
-assert function[0](10) == 10
-levels = [0,1,4,9]
-function = polyfit(dates,levels, 2)
-assert math.isclose(function[0](10),100, rel_tol=0.0001)
+def test_polyfit():
+    dates = [datetime.datetime(2020, 5, 17), datetime.datetime(2020, 5, 18),datetime.datetime(2020, 5, 19),datetime.datetime(2020, 5, 20) ]
+    levels=[0,1,2,3]
+    function = polyfit(dates,levels, 1)
+    assert function[0](10) == 10
+    levels = [0,1,4,9]
+    function = polyfit(dates,levels, 2)
+    assert math.isclose(function[0](10),100, rel_tol=0.0001)
+test_polyfit()
--- test_flood.py
+++ test_flood.py
@@ -1,6 +1,7 @@
 from floodsystem.station import MonitoringStation
 import floodsystem.geo
 from floodsystem.Flood import stations_level_over_threshold
+from floodsystem.Flood import stations_highest_rel_level
 def test_statoverthresh():
     station1= MonitoringStation(0,0,"station 1",(0,0),(1.2,2.2),"river 1","town 1")
     station2= MonitoringStation(0,0,"station 2",(10,0),(None,0),"river 1","town 2")
@@ -8,4 +9,13 @@ def test_statoverthresh():
     assert stations_level_over_threshold([station1,station2],0.8)==[(station1, 1.0)]
     station1.latest_level=1.2
     assert stations_level_over_threshold([station1,station2],0.8)==[]
+def test_stations_highest_rel_level():
+    station1= MonitoringStation(0,0,"station 1",(0,0),(1.2,2.2),"river 1","town 1")
+    station2= MonitoringStation(0,0,"station 2",(10,0),(None,0),"river 1","town 2")
+    station1.latest_level=2.2
+    station2.latest_level=1.2
+    highest=stations_highest_rel_level([station1,station2],1)
+    assert highest[0].name =="station 1"
+    
 test_statoverthresh()
+test_stations_highest_rel_level()
